ab26300ecc8a5f479d8ab2130ee207ab5d377a23
Epiko1994 <Epiko1994@gmail.com>
2019-04-04 12:06:22 +0200

    Joshua har committet sin første del af sin Branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        udskriv("Joshua");
+
     }
+
+    public static void udskriv(String hilsen) {
+
+        System.out.println(hilsen);
+
+    }
+
 }
